17e2f822d97695e321e2ba33d8dd8d992b27d54d
STEVEN A BOWMAN <stevenabowman@Mac.lan>
2025-01-12 00:21:54 -0500

    Hero Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,8 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">Steven Bowman <span className="sm:block hidden">| Ella Tech Solutions LLC</span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+          Steven Bowman &nbsp; <span className="sm:block hidden">| Ella Tech Solutions LLC</span></p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
